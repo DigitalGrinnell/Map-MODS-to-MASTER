831f407bc3304bf75b2f33bb48acf54a07838626
McFateM <mcfatem@grinnell.edu>
2020-04-09 16:13:46 -0500

    Updated constant.py

--- constant.py
+++ constant.py
@@ -2,7 +2,8 @@ DONE = ' ==> '
 HREF = 'https://digital.grinnell.edu/islandora/object/'
 DEBUG = False
 
-COLLECTIONS_PATH = '/Volumes/DGIngest/Metadata-Review-2020/xml-working-copy/*'
+COLLECTIONS_PATH = '/Volumes/LIBRARY/ALLSTAFF/DG-Metadata-Review-2020-r1/*'
+# COLLECTIONS_PATH = '/Volumes/DGIngest/Metadata-Review-2020/xml-working-copy/*'
 # COLLECTIONS_PATH = '/Volumes/DGIngest/Metadata-Review-2020/test/*'
 # COLLECTIONS_PATH = '/Users/markmcfate/GitHub/Map-MODS-to-MASTER/my_data/test/*'
 # COLLECTIONS_PATH = '/Users/markmcfate/GitHub/Map-MODS-to-MASTER/my_data/xml/*'
